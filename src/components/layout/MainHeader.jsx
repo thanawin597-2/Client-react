9b7c463f1d1e357c3b92c3557bbf0ef78982eb57
thanawin597-2 <thanawin.p@kkumail.com>
2025-10-20 22:09:50 +0700

    Update MainHeader.jsx

---
+++ src/components/layout/MainHeader.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const MainHeader = () => {
+	return (
+		<header className="header-banner">
+			<div className="overlay"></div>
+			<div className="animated-texts overlay-content">
+				<h1 className="m1">
+					Welcome to <span className="hotel-color"> love Hotel</span>
+				</h1>
+				<h4 className="m1">Experience the Best Hospitality in Town</h4>
+			</div>
+		</header>
+	)
+}
+
+export default MainHeader
